6f393e29684f7176cae0ab821f7aa95f950488b5
balkon16 <pawel.k.lonca@gmail.com>
2021-05-03 16:14:49 +0200

    Publish car engine's status to the topic.

---
+++ src/main/java/edu/pja/sri/plonca/sri3jms/model/CarStatusMessage.java
@@ -0,0 +1,29 @@
+package edu.pja.sri.plonca.sri3jms.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CarStatusMessage {
+    private static long idIndex = 0;
+
+    public static long nextId() { return idIndex++; }
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createdAt;
+    private Map<String, ?> statusMap;
+
+    private long id;
+    private String message;
+}
---
+++ src/main/java/edu/pja/sri/plonca/sri3jms/producer/CarEngineStatusTopicProducer.java
@@ -0,0 +1,61 @@
+package edu.pja.sri.plonca.sri3jms.producer;
+
+import edu.pja.sri.plonca.sri3jms.config.JmsConfig;
+import edu.pja.sri.plonca.sri3jms.model.CarStatusMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.wildfly.common.annotation.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@Component
+@RequiredArgsConstructor
+public class CarEngineStatusTopicProducer {
+    private final JmsTemplate jmsTemplate;
+
+    //    private double engineTemperature;
+//    private double oilPressure;
+//    private double brakeFluidPressure;
+//    private double rightFrontWheelPressure;
+//    private double leftFrontWheelPressure;
+//    private double rightRearWheelPressure;
+//    private double leftRearWheelPressure;
+
+    @NotNull
+    private Map<String, Double> generateEngineStatus(){
+        // TODO: zaimplementować logikę różnych poziomów (bezpieczny, ostrzeżenie, zagrożenie) dla losowo wybranych
+        //  elementów
+        Map<String, Double> carMeasurementsMap = new HashMap<>();
+        carMeasurementsMap.put("engineTemperature", 90.1);
+        carMeasurementsMap.put("oilPressure", 45.12);
+        carMeasurementsMap.put("brakeFluidPressure", 10.1);
+        carMeasurementsMap.put("rightFrontWheelPressure", 32.5);
+        carMeasurementsMap.put("leftFrontWheelPressure", 32.9);
+        carMeasurementsMap.put("rightRearWheelPressure", 31.5);
+        carMeasurementsMap.put("leftRearWheelPressure", 31.8);
+
+        return carMeasurementsMap;
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendCarEngineStatus() {
+
+        Map<String, Double> engineStatusMeasurements = generateEngineStatus();
+
+        CarStatusMessage message = CarStatusMessage.builder()
+                .id(CarStatusMessage.nextId())
+                .createdAt(LocalDateTime.now())
+                .statusMap(engineStatusMeasurements)
+                .message("Car engine status")
+                .build();
+        jmsTemplate.convertAndSend(JmsConfig.TOPIC_CAR_ENGINE_STATUS, message);
+        System.out.println("CarEngineStatusTopicProducer.sendCarEngineStatus - sent message: " + message);
+
+    }
+
+}
