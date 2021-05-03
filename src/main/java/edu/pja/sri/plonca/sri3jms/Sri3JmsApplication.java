4fec849314df1db06f0a8d436afddc2c9844faf1
balkon16 <pawel.k.lonca@gmail.com>
2021-05-03 14:25:35 +0200

    initial commit

---
+++ src/main/java/edu/pja/sri/plonca/sri3jms/Sri3JmsApplication.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.plonca.sri3jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sri3JmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sri3JmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/pja/sri/plonca/sri3jms/Sri3JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.plonca.sri3jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri3JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
